fdc10ef0e792521d38276e825b485c7d7254ec3d
chenxuewen <1107972674@qq.com>
2019-08-10 11:53:03 +0800

    新建当代青年项目

---
+++ src/main/java/com/itchan/youthguide/YouthguideApplication.java
@@ -0,0 +1,13 @@
+package com.itchan.youthguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthguideApplication.class, args);
+	}
+
+}
